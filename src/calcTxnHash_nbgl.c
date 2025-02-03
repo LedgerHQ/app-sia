209d14e0964b4a5eae3694bc5e3997953f97e050
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2025-02-03 11:43:06 +0100

    Replace nbgl_useCaseReviewStart by nbgl_useCaseReview

--- src/calcTxnHash_nbgl.c
+++ src/calcTxnHash_nbgl.c
@@ -12,79 +12,11 @@
 #include "sia.h"
 #include "sia_ux.h"
 #include "txn.h"
+#include "nbgl_use_case.h"
 
 static calcTxnHashContext_t *ctx = &global.calcTxnHashContext;
 
-static void fmtTxnElem(void);
-static uint16_t display_index(void);
-static bool nav_callback(uint8_t page, nbgl_pageContent_t *content);
-static void confirm_callback(bool confirm);
-
-// Gets the current index number to be displayed in the UI
-static uint16_t display_index(void) {
-    txn_state_t *txn = &ctx->txn;
-    uint16_t first_index_of_type = 0;
-    const txnElemType_e current_type = txn->elements[ctx->elementIndex].elemType;
-    for (uint16_t i = 0; i < txn->elementIndex; i++) {
-        if (current_type == txn->elements[i].elemType) {
-            first_index_of_type = i;
-            break;
-        }
-    }
-    return ctx->elementIndex - first_index_of_type + 1;
-}
-
-// This is a helper function that prepares an element of the transaction for
-// display. It stores the type of the element in labelStr, and a human-
-// readable representation of the element in fullStr. As in previous screens,
-// partialStr holds the visible portion of fullStr.
-static void fmtTxnElem(void) {
-    txn_state_t *txn = &ctx->txn;
-
-    switch (txn->elements[ctx->elementIndex].elemType) {
-        case TXN_ELEM_SC_OUTPUT: {
-            memmove(ctx->labelStr, "SC Output #", 11);
-            bin2dec(ctx->labelStr + 11, display_index());
-            // An element can have multiple screens. For each siacoin output, the
-            // user needs to see both the destination address and the amount.
-            // These are rendered in separate screens, and elemPart is used to
-            // identify which screen is being viewed.
-            format_address(ctx->fullStr[0], txn->elements[ctx->elementIndex].outAddr);
-            const uint8_t valLen =
-                cur2dec(ctx->fullStr[1], txn->elements[ctx->elementIndex].outVal);
-            formatSC(ctx->fullStr[1], valLen);
-            break;
-        }
-
-        case TXN_ELEM_SF_OUTPUT: {
-            memmove(ctx->labelStr, "SF Output #", 11);
-            bin2dec(ctx->labelStr + 11, display_index());
-            format_address(ctx->fullStr[0], txn->elements[ctx->elementIndex].outAddr);
-            cur2dec(ctx->fullStr[1], txn->elements[ctx->elementIndex].outVal);
-            break;
-        }
-
-        case TXN_ELEM_MINER_FEE: {
-            // Miner fees only have one part.
-            memmove(ctx->labelStr, "Miner Fee #", 11);
-            bin2dec(ctx->labelStr + 11, display_index());
-
-            const uint8_t valLen =
-                cur2dec(ctx->fullStr[0], txn->elements[ctx->elementIndex].outVal);
-            formatSC(ctx->fullStr[0], valLen);
-            break;
-        }
-
-        default:
-            // This should never happen.
-            io_send_sw(SW_DEVELOPER_ERR);
-            ui_idle();
-            break;
-    }
-}
-
 static void confirm_callback(bool confirm) {
-    ctx->finished = false;
     ctx->initialized = false;
 
     if (confirm) {
@@ -103,68 +35,59 @@ static void confirm_callback(bool confirm) {
     }
 }
 
-static nbgl_layoutTagValue_t pairs[2];
+static nbgl_contentTagValue_t *getTagValuePairs(uint8_t pairIndex) {
+    static nbgl_contentTagValue_t contentTagValue = {0};
+    txn_state_t *txn = &ctx->txn;
+    uint8_t valLen = 0;
 
-static bool nav_callback(uint8_t page, nbgl_pageContent_t *content) {
-    ctx->elementIndex = page;
-    if (ctx->elementIndex >= ctx->txn.elementIndex) {
-        content->type = INFO_LONG_PRESS;
-        content->infoLongPress.icon = &C_stax_app_sia_big;
-        if (ctx->sign) {
-            content->infoLongPress.text = "Sign transaction";
-            content->infoLongPress.longPressText = "Hold to sign";
-        } else {
-            content->infoLongPress.text = "Hash transaction";
-            content->infoLongPress.longPressText = "Hold to hash";
-        }
-        return true;
+    switch (txn->elements[ctx->elementIndex].elemType) {
+        case TXN_ELEM_SC_OUTPUT:
+            // For each siacoin output, the user needs to see both
+            // the destination address and the amount.
+            ctx->elementIndex = pairIndex / 2;
+            if (pairIndex % 2 == 0) {
+                format_address(ctx->fullStr[0], txn->elements[ctx->elementIndex].outAddr);
+                contentTagValue.item = "To";
+                contentTagValue.value = ctx->fullStr[0];
+            } else {
+                valLen = cur2dec(ctx->fullStr[1], txn->elements[ctx->elementIndex].outVal);
+                formatSC(ctx->fullStr[1], valLen);
+                contentTagValue.item = "Amount (SC)";
+                contentTagValue.value = ctx->fullStr[1];
+            }
+            break;
+
+        case TXN_ELEM_SF_OUTPUT:
+            // For each siacoin output, the user needs to see both
+            // the destination address and the amount.
+            ctx->elementIndex = pairIndex / 2;
+            if (pairIndex % 2 == 0) {
+                format_address(ctx->fullStr[0], txn->elements[ctx->elementIndex].outAddr);
+                contentTagValue.item = "To";
+                contentTagValue.value = ctx->fullStr[0];
+            } else {
+                cur2dec(ctx->fullStr[1], txn->elements[ctx->elementIndex].outVal);
+                contentTagValue.item = "Amount (SF)";
+                contentTagValue.value = ctx->fullStr[1];
+            }
+            break;
+
+        case TXN_ELEM_MINER_FEE:
+            ctx->elementIndex = pairIndex;
+            valLen = cur2dec(ctx->fullStr[0], txn->elements[ctx->elementIndex].outVal);
+            formatSC(ctx->fullStr[0], valLen);
+            contentTagValue.item = "Miner Fee Amount (SC)";
+            contentTagValue.value = ctx->fullStr[0];
+            break;
+
+        default:
+            // This should never happen.
+            io_send_sw(SW_DEVELOPER_ERR);
+            ui_idle();
+            break;
     }
 
-    fmtTxnElem();
-
-    if (ctx->txn.elements[ctx->elementIndex].elemType == TXN_ELEM_MINER_FEE) {
-        pairs[0].item = "Miner Fee Amount (SC)";
-        pairs[0].value = ctx->fullStr[0];
-
-        content->tagValueList.nbPairs = 1;
-        content->tagValueList.pairs = &pairs[0];
-    } else {
-        pairs[0].item = "To";
-        pairs[0].value = ctx->fullStr[0];
-        if (ctx->txn.elements[ctx->elementIndex].elemType == TXN_ELEM_SC_OUTPUT) {
-            pairs[1].item = "Amount (SC)";
-        } else {
-            pairs[1].item = "Amount (SF)";
-        }
-        pairs[1].value = ctx->fullStr[1];
-
-        content->tagValueList.nbPairs = 2;
-        content->tagValueList.pairs = &pairs[0];
-    }
-
-    content->title = ctx->labelStr;
-    content->type = TAG_VALUE_LIST;
-    content->tagValueList.callback = NULL;
-
-    content->tagValueList.startIndex = 0;
-    content->tagValueList.wrapping = false;
-    content->tagValueList.smallCaseForValue = false;
-    content->tagValueList.nbMaxLinesForValue = 0;
-
-    return true;
-}
-
-static void begin_review(void) {
-    nbgl_useCaseRegularReview(0,
-                              ctx->txn.elementIndex + 1,
-                              "Cancel",
-                              NULL,
-                              nav_callback,
-                              confirm_callback);
-}
-
-static void cancel_review(void) {
-    confirm_callback(false);
+    return &contentTagValue;
 }
 
 static void zero_ctx(void) {
@@ -175,6 +98,9 @@ static void zero_ctx(void) {
 // SigHash of the transaction, and optionally signs the hash using a specified
 // key. The transaction is displayed piece-wise to the user.
 uint16_t handleCalcTxnHash(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t dataLength) {
+    nbgl_contentTagValueList_t contentTagValueList = {0};
+    uint16_t i = 0;
+
     if ((p1 != P1_FIRST && p1 != P1_MORE) || (p2 != P2_DISPLAY_HASH && p2 != P2_SIGN_HASH)) {
         return SW_INVALID_PARAM;
     }
@@ -232,12 +158,20 @@ uint16_t handleCalcTxnHash(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t
             return SW_OK;
             break;
         case TXN_STATE_FINISHED:
-            nbgl_useCaseReviewStart(&C_stax_app_sia_big,
-                                    (ctx->sign) ? "Sign Transaction" : "Hash Transaction",
-                                    NULL,
-                                    "Cancel",
-                                    begin_review,
-                                    cancel_review);
+            // Computes the number of pairs to display
+            contentTagValueList.nbPairs = 0;
+            for (i = 0; i < ctx->txn.elementIndex; i++) {
+                contentTagValueList.nbPairs +=
+                    (ctx->txn.elements[i].elemType == TXN_ELEM_MINER_FEE) ? 1 : 2;
+            }
+            contentTagValueList.callback = getTagValuePairs;
+            nbgl_useCaseReview(TYPE_TRANSACTION,
+                               &contentTagValueList,
+                               &C_stax_app_sia_big,
+                               (ctx->sign) ? "Sign Transaction" : "Hash Transaction",
+                               NULL,
+                               (ctx->sign) ? "Sign Transaction" : "Hash Transaction",
+                               confirm_callback);
             break;
     }
 
